6c2ea7e34f64e6b702319f5a5d202f33fc2eb8ec
Leonel Meque <leomeque@gmail.com>
2024-02-04 14:32:27 +0100

    feat: add two new sections

--- timelinIOS/Views/organisms/RecentlyChangedSectionView.swift
+++ timelinIOS/Views/organisms/RecentlyChangedSectionView.swift
@@ -1,8 +1,61 @@
-//
-//  RecentlyChangedSectionVIew.swift
-//  timelinapp
-//
-//  Created by Leonel Meque on 3/2/24.
-//
+import UIKit
 
-import Foundation
+class RecentlyChangedSectionView: UIView {
+
+  var headerSection = TLTypography(title: "Recently Changed", fontSize: .title, colour: .label, weight: .bold)
+  var recentlyChangedTodo: TLTodoCard = TLTodoCard()
+  private lazy var recentlyChangedTodoId: String = ""
+
+  override init(frame: CGRect) {
+      super.init(frame: frame)
+      configuration()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  private func configuration(){
+    translatesAutoresizingMaskIntoConstraints = false
+    addSubview(headerSection)
+
+    configureHeader()
+    fetchRecentlyChangedTodo()
+    configureRecentlyChangedTodo()
+  }
+
+  private func configureHeader() {
+    headerSection.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      headerSection.topAnchor.constraint(equalTo: topAnchor),
+      headerSection.leadingAnchor.constraint(equalTo: leadingAnchor),
+      headerSection.trailingAnchor.constraint(equalTo: trailingAnchor)
+    ])
+  }
+
+  private func configureRecentlyChangedTodo(){
+    addSubview(recentlyChangedTodo)
+    recentlyChangedTodo.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      recentlyChangedTodo.topAnchor.constraint(equalTo: headerSection.bottomAnchor, constant: TLSpacing.s8.size),
+      recentlyChangedTodo.leadingAnchor.constraint(equalTo: leadingAnchor),
+      recentlyChangedTodo.trailingAnchor.constraint(equalTo: trailingAnchor),
+      recentlyChangedTodo.heightAnchor.constraint(equalToConstant: 200)
+    ])
+  }
+
+  private func fetchRecentlyChangedTodo(){
+    Task {
+      let data = try await NetworkManager.shared.getPinnedAndRecentlyChangedTodo()
+      recentlyChangedTodoId = data?.latestChanged ?? ""
+    }
+  }
+
+  func setRecentlyChangedTodo(model: [Todo]){
+    if let todo = model.first(where: {$0.id == recentlyChangedTodoId}) {
+      DispatchQueue.main.async {
+        self.recentlyChangedTodo.setTodo(title: todo.todo, description: todo.description, colour: .pink, createdAt: Date.dateFormatter(from: Double(todo.timestamp)))
+      }
+    }
+  }
+}
