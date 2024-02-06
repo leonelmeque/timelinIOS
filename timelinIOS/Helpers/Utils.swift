0beda19817637fc6dfc7b913eec6730e5b8a9b46
Leonel Meque <leomeque@gmail.com>
2024-02-06 15:47:30 +0100

    feat: add recent update checker

--- timelinIOS/Helpers/Utils.swift
+++ timelinIOS/Helpers/Utils.swift
@@ -1,8 +1,21 @@
-//
-//  Utils.swift
-//  timelinapp
-//
-//  Created by Leonel Meque on 5/2/24.
-//
-
 import Foundation
+
+class Utils {
+  // Is important to note that the timestamp is in milliseconds 
+  static func checkIfUpdateIsRecent(with timestamp: Double) -> Bool {
+    let initialDate = Date(timeIntervalSince1970: timestamp / 1000)
+    let calendar = Calendar.current
+    let currentDate = Date()
+
+    if let diff = calendar.dateComponents([.day], from: initialDate, to: currentDate).day {
+      print(diff)
+      return diff < 5
+    }
+
+    return false
+  }
+
+  static func motivationMessage() {
+
+  }
+}
