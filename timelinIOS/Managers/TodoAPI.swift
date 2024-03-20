ab4217147fdb20905a23396e198b141b1e44fc50
Leonel Meque <leonel.meque@glovoapp.com>
2024-03-20 12:05:56 +0200

    feat: add getTodo with id

--- timelinIOS/Managers/TodoAPI.swift
+++ timelinIOS/Managers/TodoAPI.swift
@@ -42,5 +42,34 @@ extension NetworkManager {
     return data
   }
 
+  func getTodoWith(id: String) async throws -> Todo? {
+    let docRef = db.collection("todos").document("I83gWNye5ibHcaGNCzh6S4z66492").collection("list").document(id)
+    var data: Todo? = nil
+
+    do {
+      let querySnapshot = try await docRef.getDocument()
+      let decoder = JSONDecoder()
+
+      let jsonData = try JSONSerialization.data(withJSONObject: querySnapshot.data())
+      let decodedObject = try decoder.decode(Todo.self, from: jsonData)
+      data = decodedObject
+    } catch {
+      print(error)
+      return data
+    }
+
+    return data
+  }
+
+  func updateTodoWith(id: String = "tQObrDbLFDEnNcWf6zX3",with data: [String: Any]) async throws {
+    let docRef = db.collection("todos").document("I83gWNye5ibHcaGNCzh6S4z66492").collection("list").document("tQObrDbLFDEnNcWf6zX3")
+
+    do {
+      _ = try? await docRef.updateData(data)
+    } catch {
+      print(error)
+    }
+  }
+
 
 }
